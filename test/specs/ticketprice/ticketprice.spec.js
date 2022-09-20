6dd64b6266ddb8e7ed8844f016699299a1559936
mahbub095 <mahbub.095@gmail.com>
2022-09-20 12:03:54 +0600

    Test case fixed

--- test/specs/ticketprice/ticketprice.spec.js
+++ test/specs/ticketprice/ticketprice.spec.js
@@ -10,24 +10,24 @@ const westernTrainListScreen = require("../../screenobjects/ticketprice/westernt
 
 describe('Verify Train Ticket Price Feature', () => {
 
-    before(async()=>{
+    before(async () => {
         await landingScreen.clickSkipBtn()
         await registrationScreen.clickSkipRegistrationBtn()
         await homeScreen.clickTicketPriceButton()
     })
-    
-    // it('Verify user can view Western Region train ticket price',async()=>{
-    //     await driver.back()
-    //     await driver.back()
-    //     await ticketPriceHomeScreen.clickWasternTicketPriceBtn()
-    //     await westernTrainListScreen.clickFirstWesternTrain()
-    //     expect(await ticketPriceStationListScreen.isStationListExist()).toBeTruthy()
-    //     await ticketPriceStationListScreen.enterFromStationName(ticketPriceData.weasternRegion.fromStationName)
-    //     await ticketPriceStationListScreen.selectFromStation(ticketPriceData.weasternRegion.fromStationName)
-    //     await ticketPriceStationListScreen.enterToStationName(ticketPriceData.weasternRegion.ToStationName)
-    //     await ticketPriceStationListScreen.selectToStation(ticketPriceData.weasternRegion.ToStationName)
-    //     await ticketPriceStationListScreen.clickFirstStation()
-    //     expect(await ticketPriceDetailScreen.getDistance()).toContain(ticketPriceData.weasternRegion.distance)
-    //     expect(await ticketPriceDetailScreen.getShovonChairPrice()).toContain(ticketPriceData.weasternRegion.shovonChairPrice)
-    // })
+
+    it('Verify user can view Western Region train ticket price', async () => {
+        await driver.back()
+        await driver.back()
+        await ticketPriceHomeScreen.clickWasternTicketPriceBtn()
+        await westernTrainListScreen.clickFirstWesternTrain()
+        expect(await ticketPriceStationListScreen.isStationListExist()).toBeTruthy()
+        await ticketPriceStationListScreen.enterFromStationName(ticketPriceData.weasternRegion.fromStationName)
+        await ticketPriceStationListScreen.selectFromStation(ticketPriceData.weasternRegion.fromStationName)
+        await ticketPriceStationListScreen.enterToStationName(ticketPriceData.weasternRegion.ToStationName)
+        await ticketPriceStationListScreen.selectToStation(ticketPriceData.weasternRegion.ToStationName)
+        await ticketPriceStationListScreen.clickFirstStation()
+        expect(await ticketPriceDetailScreen.getDistance()).toContain(ticketPriceData.weasternRegion.distance)
+        expect(await ticketPriceDetailScreen.getShovonChairPrice()).toContain(ticketPriceData.weasternRegion.shovonChairPrice)
+    })
 });
